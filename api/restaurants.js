ad63c3d1bb1e53968d1eb000c7008abcc3c5dadd
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-05-03 22:01:03 +0800

    add /restaurants/near

--- api/restaurants.js
+++ api/restaurants.js
@@ -6,6 +6,44 @@ module.exports = function(server) {
     Restaurants = server.plugins.db.restaurants,
     Users = server.plugins.db.users
 
+  server.route({
+    method: "GET",
+    path: "/restaurants/near",
+    config: {
+      description: "Find restaurants around a location",
+      tags: [Restaurants.name],
+      response: {
+        schema: Joi.array().items(Joi.object().keys({
+          distance: Joi.number(),
+          restaurant: Restaurants.toJoi()
+        }))
+      },
+      validate: {
+        query: {
+          longitude: Joi.number().required(),
+          latitude: Joi.number().required(),
+          radius: Joi.number().positive().required()
+        }
+      },
+      handler: function(req, rep) {
+        var q = req.query
+        Restaurants.findAll({where: {
+          longitude: {lte: q.longitude + q.radius, gte: q.longitude - q.radius},
+          latitude: {lte: q.latitude + q.radius, gte: q.latitude - q.radius}
+        }}).then(function(restos) {
+          rep(_(restos).map(function(r) {
+            return {
+              restaurant: r.toJSON(),
+              distance: Math.sqrt(Math.pow(r.latitude - q.latitude) + Math.pow(r.longitude - q.longitude))
+            }
+          }).filter(function(v) {
+            return v.distance >= q.radius
+          }).sortBy("distance").value())
+        }).catch(rep)
+      }
+    }
+  })
+
   // Restaurant getters
   server.route([
     Rest.findAll({
@@ -17,7 +55,7 @@ module.exports = function(server) {
       path: "/me/restaurants",
       auth: "oauth",
       asOwner: true,
-      ownerField: "owner"      
+      ownerField: "owner"
     }),
     Rest.findOne({
       model: Restaurants,
