b4c398c57847dcbf6b5f15e10c64ae75cf9bb2cc
Jean-Baptiste Gosselin <1536838+dennajort@users.noreply.github.com>
2015-05-07 16:52:27 +0800

    add dev and prod storage

--- server/storage/index.js
+++ server/storage/index.js
@@ -1,10 +1,16 @@
 var WebDavStorage = require("./webdavstorage")
 
 exports.register = function(server, options, next) {
+  var url = "http://owncloud.dennajort.fr/remote.php/webdav/"
+  if (process.env.NODE_ENV == "production") {
+    url += "prod/"
+  } else {
+    url += "dev/"
+  }
   server.expose("store", new WebDavStorage({
     username: "foodie",
     password: "foodie",
-    url: "http://owncloud.dennajort.fr/remote.php/webdav/prod/"
+    url: url
   }))
   next()
 }
